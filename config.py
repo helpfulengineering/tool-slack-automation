6825b4606b9a188f26c86775ce3d4201dc84506c
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-03-31 18:35:54 +0200

    PEP8 config.py

--- config.py
+++ config.py
@@ -3,18 +3,17 @@ import boto3
 from unittest import mock
 from slack import WebClient
 
+
 def get_secrets():
     if os.environ.get("STAGE", "dev") == "test":
-        return {
-            "apiToken": "",
-            "signingSecret": "a"
-        }
+        return {"apiToken": "", "signingSecret": "a"}
     else:
         secret_arn = os.environ['SECRET_ARN']
         sm_client = boto3.client("secretsmanager")
         secret_value_response = sm_client.get_secret_value(SecretId=secret_arn)
         tokens = json.loads(secret_value_response['SecretString'])
         return tokens
+
 
 def get_slack_client(api_token):
     if os.environ.get("STAGE", "dev") == "test":
