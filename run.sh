9e19980ced0924094083433e569f7ab90918ef28
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-05-02 22:18:03 +0200

    Improve run script

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ cd -- "$(dirname -- "$0")"
 virtualenv python
 source python/bin/activate
 pip install -r requirements.txt
-source environment; python application.py
+
+source environment
+source python/bin/activate
+export FLASK_APP=application.py 
+python -m flask run --host=0.0.0.0 --port 80
