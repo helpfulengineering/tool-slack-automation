d1f01fdeda66a21b4993722c49a422ba2f6816c1
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-05-18 14:02:18 +0200

    Change parameter ordering

--- onboarding-assistant/code/menu/handler.js
+++ onboarding-assistant/code/menu/handler.js
@@ -1,18 +1,18 @@
 "use strict";
 
-const path = require('path');
+const path = require("path");
 const fuzzyaldrin = require("fuzzaldrin-plus");
 const data = require(path.resolve(__dirname, "./data.json"));
 
 
 module.exports.menu = async event => {
   let payload = JSON.parse(event.body.payload);
-  console.log(event)
-  console.log(payload)
   if(payload.type == "block_suggestion") {
-    let options = query(payload.value, ...payload.action_id.split(":")).map(item=>{
-      return {"value": item, "text": {"type": "plain_text", "text": item}}
-    });
+    let options = query(payload.value, ...payload.action_id.split(":")).map(
+        item => {
+          return {"value": item, "text": {"type": "plain_text", "text": item}}
+        }
+    );
     return {statusCode: 200, body: JSON.stringify({"options": options})};
   } else {
     return {statusCode: 400, body: "400"};
@@ -20,8 +20,10 @@ module.exports.menu = async event => {
 };
 
 
-function query(source, string, limit = undefined) {
+function query(string, source, limit = undefined) {
   // Not implemented yet: dynamic (external) data sources.
   let results = fuzzyaldrin.filter(data[source], string);
   return results.slice(0, limit);
 }
+
+module.exports.menu({})
